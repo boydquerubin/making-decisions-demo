521cbcd819ae740800b4e08917f90c382a407bc4
Boyd Querubin <boyd@ezlynkna.com>
2024-01-18 11:51:17 -0700

    update 2

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -18,8 +18,25 @@ let jonSnowDefense = 10
 //     console.log('Jamie Lannister has the same attack power')
 // }
 
-if(jonSnowHealth > jamieLannisterAttack) {
-    console.log "Jon snow lives"
-} else {
-    console.log "Jon snow has died"
-}
+// if(jonSnowHealth <= jamieLannisterAttack - jonSnowDefense) {
+//     console.log("Jon snow lives")
+// } else {
+//     jonSnowHealth -= (jamieLannisterAttack - jonSnowDefense)
+//     console.log(`Jon snow has ${jonSnowHealth}`)
+// }
+
+// if(jonSnowHealth +50 >= 100) {
+//     jonSnowHealth = 100
+//     console.log ("Jon is fully healed!")
+// } else {
+//     jonSnowHealth+= 50
+//     console.log (`Jon's health has been increased to ${jonSnowHealth}`)
+// }
+
+// let coinLandsHeads = false
+
+// if (coinLandsHeads === true) {
+//     console.log ("The fight continues!")
+// } else {
+//     console.log ("Jon runs away successfully")
+// }
