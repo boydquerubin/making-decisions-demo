34704038a61742b1262787dbf9eaf8e7d792ef7f
Boyd Querubin <boyd@ezlynkna.com>
2024-01-18 11:00:33 -0700

    first update

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -3,3 +3,15 @@
     and loops to play out a fight between Jon Snow and
     Jamie Lannister.
 */ 
+
+let jonSnowAttack = 25
+let jamieLannisterAttack = 35
+
+let jonSnowHealth = 100
+let jonSnowDefense = 10
+
+if(jonSnowAttack > jamieLannisterAttack){
+    console.log('Jon Snow has a greater attack power than Jamie Lannister')
+} else {
+    console.log('Jamie Lannister has a greater attack power than Jon Snow')
+}
