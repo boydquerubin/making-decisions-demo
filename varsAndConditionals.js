fa061c927e4917c9f06ff7ad2bb4d2d89c8cf47a
Boyd Querubin <boyd@ezlynkna.com>
2024-01-18 12:08:38 -0700

    update 2

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -4,11 +4,11 @@
     Jamie Lannister.
 */ 
 
-let jonSnowAttack = 25
-let jamieLannisterAttack = 35
+// let jonSnowAttack = 25
+// let jamieLannisterAttack = 35
 
-let jonSnowHealth = 100
-let jonSnowDefense = 10
+// let jonSnowHealth = 100
+// let jonSnowDefense = 10
 
 // if (jonSnowAttack > jamieLannisterAttack) {
 //     console.log ('Jon Snow has a greater attack power than Jamie Lannister')
@@ -39,4 +39,48 @@ let jonSnowDefense = 10
 //     console.log ("The fight continues!")
 // } else {
 //     console.log ("Jon runs away successfully")
+// }
+
+// backpack.push('sword')
+// backpack.push('shield')
+// backpack.push('food')
+
+// backpack.splice(0,1)
+
+// let furCoat = 'fur coat'
+
+// backpack.push(furCoat)
+
+// let warmJon = backpack.pop()
+
+// let itemCount = backpack.length
+
+// backpack.push('flint', 'blanket', 'knife', 'toothbrush')
+
+// let backpack2 = backpack.splice(3,3)
+
+// console.log(backpack2[0])
+
+// for(let i = 0; i < backpack.length; i++){
+//     console.log(backpack[i])
+// }
+
+// for(let i = 0; i <backpack2.length; i++){
+//     console.log(backpack2[i])
+// }
+
+// for(let i = 0; i < 3; i++){
+//     console.log(i)
+// }
+
+// if backpack.length >= 3{
+//     console.log('hit if')
+//     for(let i = 0; i < 3; i++){
+//             console.log(i)
+//         }
+// } else {
+//     console.log('hit else')
+//     for(let i = 0; i < backpack.length; i++){
+//             console.log(backpack[i])
+//         }
 // }
